25aaff71615736da6a4ee9c89e0f4e9733a0263f
hyeonah <kkongc7@gmail.com>
2019-08-07 10:48:48 +0900

    create & admin page

--- todos/views.py
+++ todos/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 def new(request):
     return render(request, 'new.html')
 
 def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    due_date = request.GET.get('due-date')
+   
+    todo = Todo()
+    todo.title = title
+    todo.content = content
+    todo.due_date = due_date
+    todo.save()
+
     return render(request, 'create.html')
